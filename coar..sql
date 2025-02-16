73bd5792a367d8de6dad3609d6ffbc2ba2a84aa2
Your Name <you@example.com>
2025-02-16 08:47:07 -0500

    'oki'

--- coar..sql
+++ coar..sql
@@ -17,7 +17,9 @@
 
 -- Volcando estructura de base de datos para coar
 CREATE DATABASE IF NOT EXISTS `coar` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `coar`;
+USE `coar`; 
+
+-- 'Comentario'
 
 -- Volcando estructura para tabla coar.ajuste_avanzado
 CREATE TABLE IF NOT EXISTS `ajuste_avanzado` (
